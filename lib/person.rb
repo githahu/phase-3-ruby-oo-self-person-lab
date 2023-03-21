b46576783f6b800e395867215612caca96384e6b
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-03-21 15:14:59 +0300

    Assignment done

--- lib/person.rb
+++ lib/person.rb
@@ -3,14 +3,34 @@ require 'pry'
 
 class Person
 
-    attr_reader :name
-    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name, :happiness
+    attr_accessor :bank_account, :hygiene
 
     def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
+    end
+#writer method for happiness
+    def happiness=(num)
+        @happiness= if num > 10
+            10
+            elsif num < 0
+                0
+            else
+                num
+            end     
+    end
+
+    def hygiene=(num)
+        @hygiene= if num > 10
+            10
+            elsif num < 0
+                0
+            else
+                num
+            end     
     end
     def clean?
         self.hygiene > 7
